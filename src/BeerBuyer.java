d598f73309e61f64943252dea39a234695ecbf3b
Matt Newman <matthewnewman0806@gmail.com>
2020-06-12 23:59:44 +0100

    Added the purchase of drinks

--- src/BeerBuyer.java
+++ src/BeerBuyer.java
@@ -1,4 +1,5 @@
 import org.osbot.rs07.api.Bank;
+import org.osbot.rs07.api.Dialogues;
 import org.osbot.rs07.api.map.Area;
 import org.osbot.rs07.api.map.constants.Banks;
 import org.osbot.rs07.api.model.NPC;
@@ -44,17 +45,18 @@ public final class BeerBuyer extends Script  {
         if (!Banks.FALADOR_WEST.contains(myPlayer()) && getInventory().getAmount(995) == 0) {
             state = "Walking to bank...";
             getWalking().webWalk(Banks.FALADOR_WEST);
-        } else if (Banks.FALADOR_WEST.contains(myPlayer())){
+        } else if (Banks.FALADOR_WEST.contains(myPlayer()) && getInventory().getAmount(995) == 0) {
             state = "Banking...";
             if (!getBank().isOpen()) {
                 getBank().open();
             } else {
-                if (bank.depositAll()) {
-
-                    Sleep.sleepUntil(() -> getInventory().isEmpty(), 5000);
+                if (!getInventory().isEmpty()) {
+                    if (bank.depositAll()) {
+                        Sleep.sleepUntil(() -> getInventory().isEmpty(), 5000);
+                    }
                 }
-                if (bank.getAmount(995) >= 28) {
-                    withdrawItem(995, 28);
+                if (bank.getAmount(995) >= 84) {
+                    withdrawItem(995, 84);
                 }
 
                 if (bank.close()) {
@@ -64,6 +66,44 @@ public final class BeerBuyer extends Script  {
         } else if (getInventory().contains(995)){
             if (faladorPub.contains(myPlayer())) {
                 state = "Buying beer";
+                NPC Emily = getNpcs().closest("Emily");
+
+                if (!dialogues.inDialogue()) {
+                    if (Emily != null) {
+                        if (Emily.isVisible()) {
+                            if (Emily.interact("Talk-to")) {
+                                new ConditionalSleep(random(1000, 2000)) {
+                                    public boolean condition() throws InterruptedException {
+                                        return dialogues.inDialogue();
+                                    }
+                                }.sleep();
+                            }
+                        } else {
+                            getCamera().toEntity(Emily);
+                        }
+                    }
+                } else {
+                    if (dialogues.isPendingOption()) {
+                        dialogues.completeDialogue("I'll try the Mind Bomb");
+                        sleep(random(400, 900));
+                    }
+                    if (dialogues.isPendingContinuation()) {
+                        dialogues.clickContinue();
+                        sleep(random(400, 900));
+                    }
+                }
+
+//                if (Emily.exists()) {
+//                    if (Emily.isVisible() && !dialogues.inDialogue()) {
+//                        Emily.interact("Talk-to");
+//                        return  dialogues.inDialogue();
+//                    } else if (dialogues.isPendingContinuation()) {
+//                            dialogues.clickContinue();
+//                        } else if (getDialogues().isPendingOption()) {
+//                             getDialogues().selectOption("I'll try the Mind Bomb");
+//                         }
+//                }
+
 
             } else {
                 state = "Walking to Pub..";
